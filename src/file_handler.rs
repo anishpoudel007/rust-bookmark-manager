44d61aae90c36206de40b84a8027067b7d465c79
Anish Poudel <anishpoudel007@gmail.com>
2024-08-18 13:00:04 +0545

    create bookmark list command.

--- src/file_handler.rs
+++ src/file_handler.rs
@@ -1,4 +1,7 @@
-use std::{fs::OpenOptions, io::Write};
+use std::{
+    fs::{File, OpenOptions},
+    io::{Read, Write},
+};
 
 pub fn save_bookmark(content: String) -> bool {
     let file_path = "/home/anishpoudel/delete-me-later/bm.txt";
@@ -12,4 +15,17 @@ pub fn save_bookmark(content: String) -> bool {
     let content = format!("{}\n", content);
 
     file.write_all(content.as_bytes()).is_ok()
+}
+
+pub fn list_bookmark() {
+    let file_path = "/home/anishpoudel/delete-me-later/bm.txt";
+
+    let mut file = File::open(file_path).expect("File not found");
+
+    let mut content = String::new();
+
+    file.read_to_string(&mut content)
+        .expect("Could not read file");
+
+    println!("{}", content);
 }
--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,24 @@ fn main() {
         .about("This program generates greeting card for the user")
         .subcommand(
             Command::new("create")
-                .about("Create subcommand")
+                .about("Create bookmark entry")
                 .arg(Arg::new("url").help("URL to bookmark").required(true)),
         )
+        .subcommand(Command::new("list").about("List bookmarks"))
         .get_matches();
 
     if let Some(matches) = matches.subcommand_matches("create") {
         match matches.get_one::<String>("url") {
             Some(url) => {
-                println!("URL is {}", url);
                 file_handler::save_bookmark(url.clone());
             }
             None => {
                 println!("NOne");
             }
         }
+    }
+
+    if let Some(matches) = matches.subcommand_matches("list") {
+        file_handler::list_bookmark();
     }
 }
